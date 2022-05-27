785016878b7b2cb9a9ee544f33f878f62555336a
dwiwidianto <dwiwidianto00@gmail.com>
2022-05-27 15:16:36 +0700

    column phone -> unique

--- database/migrations/2022_05_27_074511_add_phone_to_users.php
+++ database/migrations/2022_05_27_074511_add_phone_to_users.php
@@ -14,7 +14,7 @@ class AddPhoneToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->unique();
+            $table->string('phone')->unique()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddPhoneToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->unique();
+            $table->string('phone')->change();
         });
     }
 }
