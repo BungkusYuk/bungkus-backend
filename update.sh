c546b17e28e8344f1733e4430339053144150e0e
dwiwidianto <dwiwidianto00@gmail.com>
2022-05-29 23:57:17 +0700

    fix cicd

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+git pull origin main
+
 docker build -t registry.gitlab.com/gitops-widi/container-registry/segarbox/laravel:php8.0-fpm .
 docker push registry.gitlab.com/gitops-widi/container-registry/segarbox/laravel:php8.0-fpm
+docker service update --force segarbox_app
+
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
 
